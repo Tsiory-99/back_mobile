45aebc321332e3391bad6998dd3f7cef35a9d07a
Tsiory-99 <157091236+Tsiory-99@users.noreply.github.com>
2026-06-30 10:48:07 +0300

    Update deps.py

--- app/routers/deps.py
+++ app/routers/deps.py
@@ -1,12 +1,11 @@
 from fastapi import Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer
-from jose import jwt, JWTError
+import jwt
+from jwt import PyJWTError as JWTError
 from google.cloud.firestore import Client
 from app.core.config import settings
 from app.core.database import get_firestore
-
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl=f"{settings.API_V1_STR}/auth/login")
-
 def get_current_user(token: str = Depends(oauth2_scheme), firestore_client: Client = Depends(get_firestore)):
     credentials_exception = HTTPException(
         status_code=status.HTTP_401_UNAUTHORIZED,
